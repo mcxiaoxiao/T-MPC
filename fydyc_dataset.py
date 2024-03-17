9c52e6b7dd7fa0afb1a8ed34d74d031a17507aad
OrlosZiming <2665923759@qq.com>
2024-03-17 19:00:34 +0800

    feat:new demo

--- fydyc_dataset.py
+++ fydyc_dataset.py
@@ -3,7 +3,6 @@ from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
-# 定义自定义数据集类
 class FydycDataset(Dataset):
     def __init__(self, csv_file, transform=None):
         # 加载CSV文件
@@ -16,11 +15,12 @@ class FydycDataset(Dataset):
 
         # 提取特征和标签
         labels = self.data['实际值'].values.reshape(-1, 1)
-        features = self.data['预测值'].values.reshape(-1, 1)
+        # 现在features包括两个特征：预测值和间隔天数
+        features = self.data[['预测值', '间隔天数']].values
 
         # 训练标准化器并应用于特征和标签
-        self.data['预测值'] = self.scaler_features.fit_transform(features)
-        self.data['实际值'] = self.scaler_labels.fit_transform(labels)
+        self.features = self.scaler_features.fit_transform(features)
+        self.labels = self.scaler_labels.fit_transform(labels)
 
     def __len__(self):
         # 返回数据集中的样本数量
@@ -28,10 +28,8 @@ class FydycDataset(Dataset):
 
     def __getitem__(self, idx):
         # 从数据集中获取一项数据
-        sample = self.data.iloc[idx]
-        # 提取特征和标签
-        features = torch.tensor(sample['预测值'], dtype=torch.float32)
-        label = torch.tensor(sample['预测值'], dtype=torch.float32)
+        features = torch.tensor(self.features[idx], dtype=torch.float32)
+        label = torch.tensor(self.labels[idx], dtype=torch.float32)
         return features, label
 
     def inverse_transform_features(self, values):
@@ -40,5 +38,4 @@ class FydycDataset(Dataset):
 
     def inverse_transform_labels(self, values):
         # 逆向标准化标签
-        return self.scaler_labels.inverse_transform(values)
-
+        return self.scaler_labels.inverse_transform(values.reshape(-1, 1))
